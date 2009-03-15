24eefcdd882436379f3f14696a0fedd137d8a6d3
peter@rybin.spb.ru <peter@rybin.spb.ru@1b3945c8-faff-11dd-8c4e-4bbb75a408b7>
2009-03-15 18:51:22 +0000

    Fix formatting

--- trunk/samples_v1_src/ru.spb.rybin.ohl.v1.samples/src/ru/spb/rybin/ohl/v1/samples/covariant/functions/Function.java
+++ trunk/samples_v1_src/ru.spb.rybin.ohl.v1.samples/src/ru/spb/rybin/ohl/v1/samples/covariant/functions/Function.java
@@ -1,6 +1,6 @@
 package ru.spb.rybin.ohl.v1.samples.covariant.functions;
 
 public interface Function extends GeneralizedFunction {
-  @Override // note, covariant return type
+  @Override // note, this is a covariant return type
   Value.case getValue(float x);
 }
--- trunk/samples_v1_src/ru.spb.rybin.ohl.v1.samples/src/ru/spb/rybin/ohl/v1/samples/covariant/functions/SingularValue.java
+++ trunk/samples_v1_src/ru.spb.rybin.ohl.v1.samples/src/ru/spb/rybin/ohl/v1/samples/covariant/functions/SingularValue.java
@@ -1,5 +1,5 @@
 package ru.spb.rybin.ohl.v1.samples.covariant.functions;
 
 public interface SingularValue {
-
+  // something interesting should go here
 }
