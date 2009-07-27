d2a54985835b86ad2072c573daf19226412d9363
peter <peter@rybin.spb.ru>
2009-07-27 02:50:48 +0000

    switch to ohl 3 syntax

---
+++ samples/ru.spb.rybin.ohl.v2.samples/src/ru/spb/rybin/ohl/v2/samples/simple/Simple.java
@@ -0,0 +1,34 @@
+package ru.spb.rybin.ohl.v2.samples.simple;
+
+public class Simple {
+  static class A implements case {
+    void doAOperation() {
+    }
+  }
+  
+  static class B implements case {
+    String doBOperation(String s) {
+      return s;
+    }
+  }
+  
+  static enum-case Variant {
+    A, B,
+    case unknown(String description)
+  }
+  
+  void checker(final Variant.case v) {
+    switch (v) {
+    case instanceof A {
+      v.doAOperation();
+    }
+    case instanceof B vB {
+      vB.doBOperation(null);
+    }
+    case * unknown(String description) {
+      
+    }
+    }
+  }
+  
+}
