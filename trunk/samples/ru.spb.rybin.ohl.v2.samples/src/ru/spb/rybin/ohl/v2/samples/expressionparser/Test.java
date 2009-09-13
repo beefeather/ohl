f0ef807c8812610a519f377c3fa28542f08391c7
peter@rybin.spb.ru <peter@rybin.spb.ru@1b3945c8-faff-11dd-8c4e-4bbb75a408b7>
2009-09-13 01:50:00 +0000

    fix syntax to v. 3

--- trunk/samples/ru.spb.rybin.ohl.v2.samples/src/ru/spb/rybin/ohl/v2/samples/expressionparser/Test.java
+++ trunk/samples/ru.spb.rybin.ohl.v2.samples/src/ru/spb/rybin/ohl/v2/samples/expressionparser/Test.java
@@ -4,32 +4,30 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import ru.spb.rybin.ohl.lang.EnumCaseBase;
-import ru.spb.rybin.ohl.v2.samples.expressionparser.Lexer.Tokens.Visitor;
 import ru.spb.rybin.ohl.v2.samples.expressionparser.Parser.ParserException;
 
 public class Test {
 	public static void main(String[] args) throws ParserException {
 		final List<Lexer.Tokens.case> tokens = new ArrayList<Lexer.Tokens.case>();
-		tokens.add(Lexer.Tokens.paren_open());
-		tokens.add(Lexer.Tokens.literal(5));
-		tokens.add(Lexer.Tokens.plus());
-		tokens.add(Lexer.Tokens.paren_open());
-		tokens.add(Lexer.Tokens.identifier("a"));
-		tokens.add(Lexer.Tokens.minus());
-		tokens.add(Lexer.Tokens.literal(1));
-		tokens.add(Lexer.Tokens.paren_close());
-		tokens.add(Lexer.Tokens.paren_close());
+		tokens.add(Lexer.Tokens.paren_open);
+		tokens.add(new Lexer.Tokens.literal(5));
+		tokens.add(Lexer.Tokens.plus);
+		tokens.add(Lexer.Tokens.paren_open);
+		tokens.add(new Lexer.Tokens.identifier("a"));
+		tokens.add(Lexer.Tokens.minus);
+		tokens.add(new Lexer.Tokens.literal(1));
+		tokens.add(Lexer.Tokens.paren_close);
+		tokens.add(Lexer.Tokens.paren_close);
 		
 		Lexer lexer = new Lexer() {
 			private final Iterator<Lexer.Tokens.case> it = tokens.iterator();
-			private Lexer.Tokens.case current;
+			private Lexer.TokensEx.case current;
 			{
 				advance();
 			}
 			@Override
 			public void consume() {
-				if (current == Lexer.Tokens.eos()) {
+				if (current == Lexer.TokensEx.eos) {
 					throw new RuntimeException("Already eos");
 				}
 				advance();
@@ -38,13 +36,13 @@ public class Test {
 				if (it.hasNext()) {
 					current = it.next();
 				} else {
-					current = Lexer.Tokens.eos();
+					current = Lexer.TokensEx.eos;
 				}
 			}
 				
 			@Override
-			public EnumCaseBase<? super Visitor> peek() {
-				return current;			
+			public TokensEx.case peek() {
+				return current;
 			}
 		};
 		
