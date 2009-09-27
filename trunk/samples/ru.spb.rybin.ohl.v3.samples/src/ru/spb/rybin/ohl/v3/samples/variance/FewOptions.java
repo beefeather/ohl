75d69773fc34afa1340a36e889b4defb50f331e0
peter@rybin.spb.ru <peter@rybin.spb.ru@1b3945c8-faff-11dd-8c4e-4bbb75a408b7>
2009-09-27 19:27:40 +0000

    Add a sample on variance

---
+++ trunk/samples/ru.spb.rybin.ohl.v3.samples/src/ru/spb/rybin/ohl/v3/samples/variance/FewOptions.java
@@ -0,0 +1,5 @@
+package ru.spb.rybin.ohl.v3.samples.variance;
+
+public enum-case FewOptions {
+  case doNothing()
+}
---
+++ trunk/samples/ru.spb.rybin.ohl.v3.samples/src/ru/spb/rybin/ohl/v3/samples/variance/ManyOptions.java
@@ -0,0 +1,6 @@
+package ru.spb.rybin.ohl.v3.samples.variance;
+
+public enum-case ManyOptions extends FewOptions {
+  case goToWork(),
+  case goToCinema(String movie)
+}
