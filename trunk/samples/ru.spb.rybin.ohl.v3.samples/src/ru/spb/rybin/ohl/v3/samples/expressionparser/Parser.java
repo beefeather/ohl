53724347189377bb0cb77fd10fc43c25b8a26ed5
peter@rybin.spb.ru <peter@rybin.spb.ru@1b3945c8-faff-11dd-8c4e-4bbb75a408b7>
2009-10-12 22:46:33 +0000

    tabs -> spaces

--- trunk/samples/ru.spb.rybin.ohl.v3.samples/src/ru/spb/rybin/ohl/v3/samples/expressionparser/Parser.java
+++ trunk/samples/ru.spb.rybin.ohl.v3.samples/src/ru/spb/rybin/ohl/v3/samples/expressionparser/Parser.java
@@ -4,7 +4,7 @@ import ru.spb.rybin.ohl.v3.samples.expressionparser.Lexer.TokensEx;
 
 public class Parser {
   public Parser(Lexer lexer) {
-	  this.lexer = lexer;
+      this.lexer = lexer;
   }
   
   AstNode parse() throws ParserException {
@@ -12,96 +12,96 @@ public class Parser {
     if (lexer.peek() != TokensEx.eos) {
       throw new ParserException("EOS expected");
     }
-	  return result;
+      return result;
   }
   private AstNode parseExpression() throws ParserException {
-	  final AstNode left = parseTerminal();
-	  final AstBinaryOperation.Operation.case operation;
+      final AstNode left = parseTerminal();
+      final AstBinaryOperation.Operation.case operation;
 
-	  switch (lexer.peek()) {
-  	  case * plus() {
-  	    operation = AstBinaryOperation.Operation.plus;
-  	  }
-  	  case * minus() {
+      switch (lexer.peek()) {
+      case * plus() {
+        operation = AstBinaryOperation.Operation.plus;
+      }
+      case * minus() {
         operation = AstBinaryOperation.Operation.minus;
-  	  }
-  	  default * {
-  		  return left;
-  	  }
-	  }
-	  lexer.consume();
-	  final AstNode right = parseExpression();
+      }
+      default * {
+          return left;
+      }
+      }
+      lexer.consume();
+      final AstNode right = parseExpression();
 
-	  return new AstBinaryOperation() {
-		  public AstNode getLeft() {
-  			return left;
-  		}
-  	  public AstNode getRight() {
-  			return right;
-  		}		  
-  	  @Override
-  	  public AstType.case getSubtype() {
-  	    return this;
-  	  }
+      return new AstBinaryOperation() {
+          public AstNode getLeft() {
+            return left;
+        }
+      public AstNode getRight() {
+            return right;
+        }         
+      @Override
+      public AstType.case getSubtype() {
+        return this;
+      }
       public Operation.case getOperation() {
         return operation;
       }
-	  };
+      };
   }
 
   private AstNode parseTerminal() throws ParserException {
-	  switch (lexer.peek()) {
-  	  case * paren_open() {
-  		 lexer.consume();
-  		 AstNode inner = parseExpression();
-  		 if (lexer.peek() != Lexer.Tokens.paren_close) {
-      	 throw new ParserException("')' expected");
-  		 }
-  		 lexer.consume();
-  		 return inner;
-  	  }
-  	  case * literal(final int value) {
-  		  lexer.consume();
-  		  return new AstConstant() {
-  			  public int getValue() {
-  				  return value;
-  			  }
-		      public AstType.case getSubtype() {
-    		    return this;
-		      }
-  		  };
-  	  }
-  	  case * identifier(final String name) {
-  		  lexer.consume();
-  		  return new AstVariable() {
-  			  public String getName() {
-  				  return name;
-  			  }
-		      public AstType.case getSubtype() {
+      switch (lexer.peek()) {
+      case * paren_open() {
+         lexer.consume();
+         AstNode inner = parseExpression();
+         if (lexer.peek() != Lexer.Tokens.paren_close) {
+         throw new ParserException("')' expected");
+         }
+         lexer.consume();
+         return inner;
+      }
+      case * literal(final int value) {
+          lexer.consume();
+          return new AstConstant() {
+              public int getValue() {
+                  return value;
+              }
+              public AstType.case getSubtype() {
+                return this;
+              }
+          };
+      }
+      case * identifier(final String name) {
+          lexer.consume();
+          return new AstVariable() {
+              public String getName() {
+                  return name;
+              }
+              public AstType.case getSubtype() {
             return this;
-		      }
-  		  };
-  	  }
-  	  default * {
-       	 throw new ParserException("integer or identifier or parenthized expression expected");
-  	  }
-	  }
+              }
+          };
+      }
+      default * {
+         throw new ParserException("integer or identifier or parenthized expression expected");
+      }
+      }
   }
   
   private final Lexer lexer;
   
   public static class ParserException extends Exception {
-  	public ParserException() {
-  		super();
-  	}
-  	public ParserException(String message, Throwable cause) {
-  		super(message, cause);
-  	}
-  	public ParserException(String message) {
-  		super(message);
-  	}
-  	public ParserException(Throwable cause) {
-  		super(cause);
-  	}
+    public ParserException() {
+        super();
+    }
+    public ParserException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public ParserException(String message) {
+        super(message);
+    }
+    public ParserException(Throwable cause) {
+        super(cause);
+    }
   }
 } 
