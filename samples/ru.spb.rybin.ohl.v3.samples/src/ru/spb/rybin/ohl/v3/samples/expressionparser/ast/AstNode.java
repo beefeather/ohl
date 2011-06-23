8a50efc4a68c64f03458e27dc7bfb96688cc9175
peter <peter@rybin.spb.ru>
2011-06-23 21:58:38 +0000

    split expressionparser into a several packages

---
+++ samples/ru.spb.rybin.ohl.v3.samples/src/ru/spb/rybin/ohl/v3/samples/expressionparser/ast/AstNode.java
@@ -0,0 +1,5 @@
+package ru.spb.rybin.ohl.v3.samples.expressionparser.ast;
+
+public interface AstNode {
+  AstType.case getSubtype();
+}
---
+++ samples/ru.spb.rybin.ohl.v3.samples/src/ru/spb/rybin/ohl/v3/samples/expressionparser/ast/AstType.java
@@ -0,0 +1,7 @@
+package ru.spb.rybin.ohl.v3.samples.expressionparser.ast;
+
+public enum-case AstType {
+  AstBinaryOperation,
+  AstVariable,
+  AstConstant
+}
